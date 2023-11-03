43cdde7f37601eafdcb24478b545130231fea47e
twopats <lpatipat@icloud.com>
2023-11-03 10:30:28 -0400

    clean: foundry libs

--- script/DeployAuditorRegistry.s.sol
+++ script/DeployAuditorRegistry.s.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.19 <0.9.0;
-
-import {Script} from 'forge-std/Script.sol';
-import { AuditorRegistry } from "../src/AuditorRegistry.sol";
-
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract DeployAuditorRegistry is Script {
-    function run() public returns (AuditorRegistry ar) {
-        uint256 deployerPrivateKey = vm.envUint('PRIVATE_KEY');
-        vm.startBroadcast(deployerPrivateKey);
-        ar = new AuditorRegistry();
-        vm.stopBroadcast();
-    }
-}
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity >=0.8.19 <0.9.0;
+//
+// import {Script} from 'forge-std/Script.sol';
+// import { AuditorRegistry } from "../src/AuditorRegistry.sol";
+//
+// /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
+// contract DeployAuditorRegistry is Script {
+//     function run() public returns (AuditorRegistry ar) {
+//         uint256 deployerPrivateKey = vm.envUint('PRIVATE_KEY');
+//         vm.startBroadcast(deployerPrivateKey);
+//         ar = new AuditorRegistry();
+//         vm.stopBroadcast();
+//     }
+// }
